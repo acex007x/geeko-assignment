739d41a57d5a3a89d057f30c19e30934979fd510
Sachin Pandey <acex007x@gmail.com>
2020-11-23 07:29:20 +0530

    package json updated

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/assignmentGeeko', {
+mongoose.connect('mongodb://127.0.0.1:27017/assignmentheeko', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
